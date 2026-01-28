35fb92b81061cf2f3f9270593a67c1a05fabf5c9
Tae0072 <rkdxodh41@gmail.com>
2026-01-28 14:30:45 +0900

    1단계 세팅

---
+++ src/main/java/com/example/springv1_test/board/BoardController.java
@@ -0,0 +1,12 @@
+package com.example.springv1_test.board;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class BoardController {
+
+    public String index() {
+        return "index";
+    }
+
+}
