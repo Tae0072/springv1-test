1d838a82405a1a68ec268b8bb8fb24a6e6878017
Tae0072 <rkdxodh41@gmail.com>
2026-01-28 14:20:46 +0900

    준비

---
+++ src/main/java/com/example/springv1_test/Springv1TestApplication.java
@@ -0,0 +1,13 @@
+package com.example.springv1_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springv1TestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springv1TestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springv1_test/Springv1TestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springv1_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springv1TestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
